2cf782f72f2622cd1e0a44f2a468d016b0981f76
Jay Quach <kod88vn@gmail.com>
2019-11-21 15:12:01 -0500

    fix a test

--- src/app/models/address.spec.ts
+++ src/app/models/address.spec.ts
@@ -2,6 +2,6 @@ import { Address } from './address';
 
 describe('Address', () => {
   it('should create an instance', () => {
-    expect(new Address()).toBeTruthy();
+    expect(new Address('nothing, is, good')).toBeTruthy();
   });
 });
